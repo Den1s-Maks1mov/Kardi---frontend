39ee38868194054f0e3ee1ec233ffcf654e60d39
Den1s_Maks1mov <maksymov.denys@student.sumdu.edu.ua>
2026-08-12 20:21:40 +0300

    fix: update angular framework

---
+++ .angular/cache/20.3.25/project-shortmed/vite/deps/@angular_cdk_portal.js
@@ -0,0 +1,29 @@
+import {
+  BasePortalOutlet,
+  CdkPortal,
+  CdkPortalOutlet,
+  ComponentPortal,
+  DomPortal,
+  DomPortalOutlet,
+  Portal,
+  PortalHostDirective,
+  PortalModule,
+  TemplatePortal,
+  TemplatePortalDirective
+} from "./chunk-CLPLB6DZ.js";
+import "./chunk-M7XTDJL5.js";
+import "./chunk-RSS3ODKE.js";
+import "./chunk-H2SRQSE4.js";
+export {
+  BasePortalOutlet,
+  CdkPortal,
+  CdkPortalOutlet,
+  ComponentPortal,
+  DomPortal,
+  DomPortalOutlet,
+  Portal,
+  PortalHostDirective,
+  PortalModule,
+  TemplatePortal,
+  TemplatePortalDirective
+};
---
+++ .angular/cache/20.3.25/project-shortmed/vite/deps_ssr/@angular_cdk_portal.js
@@ -0,0 +1,30 @@
+import { createRequire } from 'module';const require = createRequire(import.meta.url);
+import {
+  BasePortalOutlet,
+  CdkPortal,
+  CdkPortalOutlet,
+  ComponentPortal,
+  DomPortal,
+  DomPortalOutlet,
+  Portal,
+  PortalHostDirective,
+  PortalModule,
+  TemplatePortal,
+  TemplatePortalDirective
+} from "./chunk-33ZKOC4H.js";
+import "./chunk-VXUMJGL5.js";
+import "./chunk-O5J3CNTX.js";
+import "./chunk-6DU2HRTW.js";
+export {
+  BasePortalOutlet,
+  CdkPortal,
+  CdkPortalOutlet,
+  ComponentPortal,
+  DomPortal,
+  DomPortalOutlet,
+  Portal,
+  PortalHostDirective,
+  PortalModule,
+  TemplatePortal,
+  TemplatePortalDirective
+};
